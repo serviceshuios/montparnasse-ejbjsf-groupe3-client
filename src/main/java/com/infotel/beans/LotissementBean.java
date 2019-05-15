150528ece410c0d94cf635592b6f4313e6638acc
Raph <raphael_bruneau@orange.fr>
2019-05-15 15:03:44 +0200

    Push des classes Beans complètes

--- src/main/java/com/infotel/beans/LotissementBean.java
+++ src/main/java/com/infotel/beans/LotissementBean.java
@@ -7,7 +7,9 @@ import javax.ejb.EJB;
 import javax.enterprise.context.SessionScoped;
 import javax.inject.Named;
 
+import com.infotel.ejb.ILotissementRemote;
 import com.infotel.metier.Lotissement;
+import com.infotel.metier.Personne;
 
 	@Named
 	@SessionScoped
@@ -46,4 +48,53 @@ import com.infotel.metier.Lotissement;
 		
 
 		// Méthodes
+		
+		public void ajouterPersonne(Personne p) {
+			service.ajouterPersonne(p);
+		}
+		public List<Personne> findAllPersonne() {
+			return service.findAllPersonne();
+		}
+		public void modifierPersonne(Personne p) {
+			service.modifierPersonne(p);
+		}
+		public void supprimerPersonne(Personne p) {
+			service.supprimerPersonne(p);
+		}
+		public Personne getPersonne(Long idPersonne) {
+			return service.getPersonne(idPersonne);
+		}
+		public Personne affichagePersonne(Long idPersonne) {
+			return service.affichagePersonne(idPersonne);
+		}
+		
+		
+		
+		public void ajouterLotissement(Lotissement l) {
+			service.ajouterLotissement(l);
+		}
+		public List<Lotissement> findAllLotissement() {
+			return service.findAllLotissement();
+		}
+		public void modifierLotissement(Lotissement l) {
+			service.modifierLotissement(l);
+		}
+		public void supprimerLotissement(Lotissement l) {
+			service.supprimerLotissement(l);
+		}
+		public Lotissement getLotissement(Long idLot) {
+			return service.getLotissement(idLot);
+		}
+		public Lotissement affichageLotissement(Long idLot) {
+			return service.affichageLotissement(idLot);
+		}
+		
+		
+		
+		public void acheterLotissement(Personne p, Lotissement l) {
+			service.acheterLotissement(p, l);
+		}
+		public void seMarier (Personne p1, Personne p2) {
+			service.seMarier(p1, p2);
+		}
 }
